5474d46e82ddb6b7ecc5f8e32a42540913886925
zrussell <zrussell1@SI-DSTEIL-1.harding.edu>
2015-04-08 12:55:31 -0500

    Added google api support

--- content.js
+++ content.js
@@ -1,16 +1,26 @@
 //content script
+var esly = {
+	classarr: []
+}
 
-var classarr = []
-var classlist = $(".container .box:nth-child(2) tr:nth-child(2) .box ul");
-debugger;
-for (var i = 0; i < classlist[0].childElementCount; i++){ 
-	classarr[i] = classlist[0].children[i].children[0].text;
+esly.classlist = $(".container .box:nth-child(2) tr:nth-child(2) .box ul");
+
+for (var i = 0; i < esly.classlist[0].childElementCount; i++){ 
+	esly.classarr[i] = esly.classlist[0].children[i].children[0].text;
 }
 
 
 var port = chrome.runtime.connect({name: "easelport"});
 port.postMessage({request: "connect"});
 port.onMessage.addListener(function(msg) {
-  if (msg.request == "getclasses")
-    port.postMessage({answer: classarr, response: "classes"});
+  if (msg.request == "getclasses"){
+    port.postMessage({answer: esly.classarr, response: "classes"});
+  }
+  else if (msg.request == "getAssignments"){
+  	//get the assignments
+
+  	//return object
+  	
+  }
+
 });
--- easely.js
+++ easely.js
@@ -1,27 +1,82 @@
+var esly = {
+  clientId: '693163061755-3imno95ak523h2s90vur7h9srfk5s7b4.apps.googleusercontent.com',
+  apiKey: 'AIzaSyDVBLlhB2ExPs7GrVPMz8DRwJunKhZEx1Y',
+  scopes: 'https://www.googleapis.com/auth/calendar',
+  getCookie: function(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+  },
+  askForClassAssignmentArr:function(classarr){
+    port.postMessage({request: "getAssignments", classes: classarr});
+  },
+  addToCalendar: function(event){
+    gapi.client.load('calendar', 'v3', function() {
+      var request = gapi.client.calendar.events.list({
+        'calendarId': 'primary'
+      });
+            
+      request.execute(function(resp) {
+        console.log(resp)
+      });
+    });
+  },
+  initapi: function(){
+    gapi.client.setApiKey(esly.apiKey);
+    window.setTimeout(esly.checkAuth,1);
+    esly.checkAuth();
+  },
+  checkAuth: function() {
+    gapi.auth.authorize({client_id: esly.clientId, 
+                         cookie_policy: 'single_host_origin',
+                         scope: esly.scopes, 
+                         immediate: true},
+                         esly.handleAuthResult);
+  },
+  handleAuthResult: function(authResult) {
+    if (authResult) {
+      console.log(authResult)
+      //esly.addToCalendar();
+    } 
+  },
+  handleAuthClick: function(event) {
+    gapi.auth.authorize(
+        {client_id: clientId, scope: scopes, immediate: false},
+        esly.handleAuthResult);
+    return false;
+  }
+}
+
 $(document).ready(function(){
 
   chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == "easelport");
-  port.onMessage.addListener(function(msg) {
-    if(msg.request == "connect"){
-      console.log("connectectt");
-      port.postMessage({request: "getclasses"});
-    }
-    else if(msg.response == "classes" && $('#classesView').css('display') == 'none'){
-      console.log(msg.answer);
-
-      var table = $('<table></table>').addClass('classtable');
-      for(var i = 0; i < msg.answer.length; i++){
-          var row = $('<tr></tr>').addClass('enrolledclass').text(msg.answer[i]);
-          table.append(row);
+    console.assert(port.name == "easelport");
+    port.onMessage.addListener(function(msg) {
+      if(msg.request == "connect"){
+        port.postMessage({request: "getclasses"});
       }
-      $('#classesView').append(table);
-      $("#genInfo").text('Select the classes you wish to add to Google Calendar');
-      $("#loginForm").hide();
-      $("#classesView").show();
-    }
+      else if(msg.response == "classes" && 
+            $('#classesView').css('display') == 'none' &&
+            $('.classtable').length == 0){
+
+        esly.table = $('<table></table>').addClass('classtable');
+        for(var i = 0; i < msg.answer.length; i++){
+            var $row = $("<tr id='classindex-"+ i +"'></tr>").addClass('enrolledclass').text(msg.answer[i]);
+            $row.prepend("<input type='radio' id='class-radio-" + i+ "'>")
+            esly.table.append($row);
+        }
+        $('#classesView').append(esly.table);
+        $('#classesView').append("<button id='addToCalButton'>Add To Calendar</button>")
+        $('#addToCalButton').click(function(){esly.askForClassAssignmentArr(msg.answer.length)});
+        $("#genInfo").text('Select the classes you wish to add to Google Calendar');
+        $("#loginForm").hide();
+        $("#classesView").show();
+      }
+      else if(msg.response =="assignments"){
+        console.log(msg.answer)
+      }
+    });
   });
-});
 
   $("#submitButton").click(function(){
     chrome.tabs.update({
@@ -36,14 +91,3 @@ $(document).ready(function(){
     });
   });
 })
-
-
-function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
-}
-
-function getClassData(){
-
-}
