5578abb8e6952874ff6edfb765db9c26d00d8828
zrussell <zrussell1@SI-DSTEIL-1.harding.edu>
2015-04-08 15:52:38 -0500

    Parsed out the class assignment data

--- content.js
+++ content.js
@@ -1,26 +1,49 @@
 //content script
 var esly = {
-	classarr: []
+	classmap: [],
+	classAssignmentArray: [],
+	addClassAssignments: function(len){
+		for (var i = 0; i < len; i++){
+			var classCal = document.getElementById('class-' + i)
+			var cal = classCal.contentDocument.body
+			esly.classAssignmentArray[i] = cal.querySelectorAll('a')
+		}
+	}
 }
 
 esly.classlist = $(".container .box:nth-child(2) tr:nth-child(2) .box ul");
 
-for (var i = 0; i < esly.classlist[0].childElementCount; i++){ 
-	esly.classarr[i] = esly.classlist[0].children[i].children[0].text;
+esly.findAllClasses = function(){
+	for (var i = 0; i < esly.classlist[0].childElementCount; i++){ 
+		esly.classmap[i] = esly.classlist[0].children[i].children[0].text;
+	}
+}
+
+esly.findAllClassAssignments = function(){
+	for (var i = 0; i < esly.classmap.length; i++){
+		var cls = document.createElement('iframe')
+		cls.src = esly.classlist[0].children[i].children[0].href
+		cls.style.display = "none"
+		cls.setAttribute("id", "class-" + i)
+		document.body.appendChild(cls) 
+	}
+	//figure out how to call this AFTER all iframes are loaded
+	setTimeout(esly.addClassAssignments(esly.classmap.length), 3000);
+	console.log(esly.classAssignmentArray)
 }
 
 
 var port = chrome.runtime.connect({name: "easelport"});
 port.postMessage({request: "connect"});
 port.onMessage.addListener(function(msg) {
-  if (msg.request == "getclasses"){
-    port.postMessage({answer: esly.classarr, response: "classes"});
-  }
-  else if (msg.request == "getAssignments"){
-  	//get the assignments
-
-  	//return object
-  	
-  }
-
+	if (msg.request == "getclasses"){
+		esly.findAllClasses()
+		port.postMessage({answer: esly.classmap, response: "classes"});
+	}
+	else if (msg.request == "getAssignments"){
+		//get the assignments
+		esly.findAllClassAssignments()
+		//return object
+		port.postMessage({answer: esly.classAssignmentArray, response: "assignments"});
+	}
 });
--- easely.js
+++ easely.js
@@ -1,43 +1,34 @@
 var esly = {
   clientId: '693163061755-3imno95ak523h2s90vur7h9srfk5s7b4.apps.googleusercontent.com',
-  apiKey: 'AIzaSyDVBLlhB2ExPs7GrVPMz8DRwJunKhZEx1Y',
   scopes: 'https://www.googleapis.com/auth/calendar',
+
   getCookie: function(name) {
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   },
-  askForClassAssignmentArr:function(classarr){
-    port.postMessage({request: "getAssignments", classes: classarr});
+  addCalendarService: function(event){
+    gapi.client.load('calendar', 'v3');
   },
-  addToCalendar: function(event){
-    gapi.client.load('calendar', 'v3', function() {
-      var request = gapi.client.calendar.events.list({
-        'calendarId': 'primary'
-      });
-            
-      request.execute(function(resp) {
-        console.log(resp)
-      });
-    });
-  },
-  initapi: function(){
-    gapi.client.setApiKey(esly.apiKey);
-    window.setTimeout(esly.checkAuth,1);
+  initapi: function(){;
     esly.checkAuth();
   },
   checkAuth: function() {
-    gapi.auth.authorize({client_id: esly.clientId, 
-                         cookie_policy: 'single_host_origin',
-                         scope: esly.scopes, 
-                         immediate: true},
+    gapi.auth.authorize({'client_id': esly.clientId,
+                         'scope': esly.scopes, 
+                         'immediate': true},
                          esly.handleAuthResult);
   },
   handleAuthResult: function(authResult) {
     if (authResult) {
-      console.log(authResult)
-      //esly.addToCalendar();
-    } 
+      console.log("auth success")
+      esly.addCalendarService();
+    } else {
+      // No access token could be retrieved, force the authorization flow.
+      gapi.auth.authorize(
+          {'client_id': esly.clientId, 'scope': esly.scopes, 'immediate': false},
+          handleAuthResult);
+    }
   },
   handleAuthClick: function(event) {
     gapi.auth.authorize(
@@ -67,10 +58,13 @@ $(document).ready(function(){
         }
         $('#classesView').append(esly.table);
         $('#classesView').append("<button id='addToCalButton'>Add To Calendar</button>")
-        $('#addToCalButton').click(function(){esly.askForClassAssignmentArr(msg.answer.length)});
+        //$('#addToCalButton').click(function(){esly.askForClassAssignmentArr(msg.answer)});
         $("#genInfo").text('Select the classes you wish to add to Google Calendar');
         $("#loginForm").hide();
         $("#classesView").show();
+
+        //ask for all class assignments
+        port.postMessage({request: "getAssignments", classes: msg.answer});
       }
       else if(msg.response =="assignments"){
         console.log(msg.answer)
@@ -82,7 +76,7 @@ $(document).ready(function(){
     chrome.tabs.update({
      url: "https://cs.harding.edu/easel/cgi-bin/proc_login?user=" + $("#username").val() + "&passwd=" + $("#passwd").val()
     }, function(tab){
-      var cookie = getCookie("EASESID");
+      var cookie = esly.getCookie("EASESID");
       if(cookie!= ""){
         //user is logged in successfully
       } else{
